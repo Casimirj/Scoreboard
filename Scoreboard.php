4e8dc7ba2cf6faeacbfbe458df033d640ff70d67
Unknown <mhm.sherpa@gmail.com>
2018-01-21 22:41:34 -0600

    added initial files

---
+++ Scoreboard.php
@@ -0,0 +1,34 @@
+<?php
+
+
+echo "helluuuu";
+
+
+/*<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="style.css" rel="stylesheet" type="text/css"/>
+
+    <title>Scores</title>
+</head>
+<body>
+<div class="background">
+    <div class="mainbox">
+
+
+
+
+
+    </div>
+
+
+</div>
+
+
+</body>
+</html>*/
+
+?>
