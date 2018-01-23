c9d242b6d78c72b06011692148efbefd39a6e884
Unknown <mhm.sherpa@gmail.com>
2018-01-22 20:33:27 -0600

    added my jetbrains tracking files to the gitignore

--- Scoreboard.php
+++ Scoreboard.php
@@ -2,33 +2,11 @@
 
 
 include('header.php');
-
-
-/*<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
-    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="style.css" rel="stylesheet" type="text/css"/>
-
-    <title>Scores</title>
-</head>
+?>
 <body>
 <div class="background">
     <div class="mainbox">
 
 
+s
 
-
-
-    </div>
-
-
-</div>
-
-
-</body>
-</html>*/
-
-?>
