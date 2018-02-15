98178658c063b1dbfd17f9fc0ee33aa34c832bcb
Unknown <mhm.sherpa@gmail.com>
2018-02-15 15:54:12 -0600

    basic formatting

--- input.php
+++ input.php
@@ -5,10 +5,115 @@ include('header.php');
 ?>
 
 
-<div class="jamesInput inputbox"></div>
-<div class="danInput inputbox"></div>
+<div class="jamesInput inputbox">
+    <input type="text" list="characters" />
+    <datalist id="characters">
+        <option>Alien</option>
+        <option>Bo Rai Cho</option>
+        <option>Cassie Cage</option>
+        <option>DVorah</option>
+        <option>Ermac</option>
+        <option>Erron Black</option>
+        <option>Ferra/Torr</option>
+        <option>Goro</option>
+        <option>Jacqui Briggs</option>
+        <option>Jason Voorhees</option>
+        <option>Jax</option>
+        <option>Johnny Cage</option>
+        <option>Kano</option>
+        <option>Kenshi</option>
+        <option>Kitana</option>
+        <option>Kotal Kahn</option>
+        <option>Kung Jin</option>
+        <option>Kung Lao</option>
+        <option>Leatherface</option>
+        <option>Liu Kang</option>
+        <option>Mileena</option>
+        <option>Predator</option>
+        <option>Quan Chi</option>
+        <option>Raiden</option>
+        <option>Reptile</option>
+        <option>Scorpion</option>
+        <option>Shinnok</option>
+        <option>Sonya Blade</option>
+        <option>Sub-Zero</option>
+        <option>Takeda</option>
+        <option>Tanya</option>
+        <option>Tremor</option>
+        <option>Triborg</option>
 
-<input type="text" list="characters" />
+    </datalist>
+
+
+
+</div>
+<div class="danInput inputbox">
+    <input type="text" list="characters" />
+    <datalist id="characters">
+        <option>Alien</option>
+        <option>Bo Rai Cho</option>
+        <option>Cassie Cage</option>
+        <option>DVorah</option>
+        <option>Ermac</option>
+        <option>Erron Black</option>
+        <option>Ferra/Torr</option>
+        <option>Goro</option>
+        <option>Jacqui Briggs</option>
+        <option>Jason Voorhees</option>
+        <option>Jax</option>
+        <option>Johnny Cage</option>
+        <option>Kano</option>
+        <option>Kenshi</option>
+        <option>Kitana</option>
+        <option>Kotal Kahn</option>
+        <option>Kung Jin</option>
+        <option>Kung Lao</option>
+        <option>Leatherface</option>
+        <option>Liu Kang</option>
+        <option>Mileena</option>
+        <option>Predator</option>
+        <option>Quan Chi</option>
+        <option>Raiden</option>
+        <option>Reptile</option>
+        <option>Scorpion</option>
+        <option>Shinnok</option>
+        <option>Sonya Blade</option>
+        <option>Sub-Zero</option>
+        <option>Takeda</option>
+        <option>Tanya</option>
+        <option>Tremor</option>
+        <option>Triborg</option>
+
+    </datalist>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--<input type="text" list="characters" />
 <datalist id="characters">
     <option>Alien</option>
     <option>Bo Rai Cho</option>
@@ -44,4 +149,4 @@ include('header.php');
     <option>Tremor</option>
     <option>Triborg</option>
 
-</datalist>
+</datalist>-->
