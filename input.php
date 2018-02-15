b25dbeda400ab54e007330e16b810efafa13926b
Unknown <mhm.sherpa@gmail.com>
2018-02-15 16:07:25 -0600

    .

--- input.php
+++ input.php
@@ -6,85 +6,111 @@ include('header.php');
 
 
 <div class="jamesInput inputbox">
-    <input type="text" list="characters" />
-    <datalist id="characters">
-        <option>Alien</option>
-        <option>Bo Rai Cho</option>
-        <option>Cassie Cage</option>
-        <option>DVorah</option>
-        <option>Ermac</option>
-        <option>Erron Black</option>
-        <option>Ferra/Torr</option>
-        <option>Goro</option>
-        <option>Jacqui Briggs</option>
-        <option>Jason Voorhees</option>
-        <option>Jax</option>
-        <option>Johnny Cage</option>
-        <option>Kano</option>
-        <option>Kenshi</option>
-        <option>Kitana</option>
-        <option>Kotal Kahn</option>
-        <option>Kung Jin</option>
-        <option>Kung Lao</option>
-        <option>Leatherface</option>
-        <option>Liu Kang</option>
-        <option>Mileena</option>
-        <option>Predator</option>
-        <option>Quan Chi</option>
-        <option>Raiden</option>
-        <option>Reptile</option>
-        <option>Scorpion</option>
-        <option>Shinnok</option>
-        <option>Sonya Blade</option>
-        <option>Sub-Zero</option>
-        <option>Takeda</option>
-        <option>Tanya</option>
-        <option>Tremor</option>
-        <option>Triborg</option>
+    <table class="inputtable">
+        <tr>
+            <td>
+                James Character
+            </td>
+            <td>
+                <input type="text" list="characters" />
+                <datalist id="characters">
+                    <option>Alien</option>
+                    <option>Bo Rai Cho</option>
+                    <option>Cassie Cage</option>
+                    <option>DVorah</option>
+                    <option>Ermac</option>
+                    <option>Erron Black</option>
+                    <option>Ferra/Torr</option>
+                    <option>Goro</option>
+                    <option>Jacqui Briggs</option>
+                    <option>Jason Voorhees</option>
+                    <option>Jax</option>
+                    <option>Johnny Cage</option>
+                    <option>Kano</option>
+                    <option>Kenshi</option>
+                    <option>Kitana</option>
+                    <option>Kotal Kahn</option>
+                    <option>Kung Jin</option>
+                    <option>Kung Lao</option>
+                    <option>Leatherface</option>
+                    <option>Liu Kang</option>
+                    <option>Mileena</option>
+                    <option>Predator</option>
+                    <option>Quan Chi</option>
+                    <option>Raiden</option>
+                    <option>Reptile</option>
+                    <option>Scorpion</option>
+                    <option>Shinnok</option>
+                    <option>Sonya Blade</option>
+                    <option>Sub-Zero</option>
+                    <option>Takeda</option>
+                    <option>Tanya</option>
+                    <option>Tremor</option>
+                    <option>Triborg</option>
 
-    </datalist>
+                </datalist>
+            </td>
+
+        </tr>
+
+    </table>
+
+
+
 
 
 
 </div>
 <div class="danInput inputbox">
-    <input type="text" list="characters" />
-    <datalist id="characters">
-        <option>Alien</option>
-        <option>Bo Rai Cho</option>
-        <option>Cassie Cage</option>
-        <option>DVorah</option>
-        <option>Ermac</option>
-        <option>Erron Black</option>
-        <option>Ferra/Torr</option>
-        <option>Goro</option>
-        <option>Jacqui Briggs</option>
-        <option>Jason Voorhees</option>
-        <option>Jax</option>
-        <option>Johnny Cage</option>
-        <option>Kano</option>
-        <option>Kenshi</option>
-        <option>Kitana</option>
-        <option>Kotal Kahn</option>
-        <option>Kung Jin</option>
-        <option>Kung Lao</option>
-        <option>Leatherface</option>
-        <option>Liu Kang</option>
-        <option>Mileena</option>
-        <option>Predator</option>
-        <option>Quan Chi</option>
-        <option>Raiden</option>
-        <option>Reptile</option>
-        <option>Scorpion</option>
-        <option>Shinnok</option>
-        <option>Sonya Blade</option>
-        <option>Sub-Zero</option>
-        <option>Takeda</option>
-        <option>Tanya</option>
-        <option>Tremor</option>
-        <option>Triborg</option>
+    <table class="inputtable">
+        <tr>
+            <td>
+                Danjamin's Character
+            </td>
+            <td>
+                <input type="text" list="characters" />
+                <datalist id="characters">
+                    <option>Alien</option>
+                    <option>Bo Rai Cho</option>
+                    <option>Cassie Cage</option>
+                    <option>DVorah</option>
+                    <option>Ermac</option>
+                    <option>Erron Black</option>
+                    <option>Ferra/Torr</option>
+                    <option>Goro</option>
+                    <option>Jacqui Briggs</option>
+                    <option>Jason Voorhees</option>
+                    <option>Jax</option>
+                    <option>Johnny Cage</option>
+                    <option>Kano</option>
+                    <option>Kenshi</option>
+                    <option>Kitana</option>
+                    <option>Kotal Kahn</option>
+                    <option>Kung Jin</option>
+                    <option>Kung Lao</option>
+                    <option>Leatherface</option>
+                    <option>Liu Kang</option>
+                    <option>Mileena</option>
+                    <option>Predator</option>
+                    <option>Quan Chi</option>
+                    <option>Raiden</option>
+                    <option>Reptile</option>
+                    <option>Scorpion</option>
+                    <option>Shinnok</option>
+                    <option>Sonya Blade</option>
+                    <option>Sub-Zero</option>
+                    <option>Takeda</option>
+                    <option>Tanya</option>
+                    <option>Tremor</option>
+                    <option>Triborg</option>
 
-    </datalist>
+                </datalist>
+            </td>
+
+        </tr>
+
+    </table>
+
 
 
 
