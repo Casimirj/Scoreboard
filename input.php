fbc7f7c1853c3bd863c590871187c1a65e515c24
Unknown <mhm.sherpa@gmail.com>
2018-02-15 15:34:08 -0600

    Simplified how site would run a lot
    Its just going to be 4 arrays for wins/losses and not a whole json file. Im still going to use data.json to back up the raw games in case my roomie starts cheatin

--- input.php
+++ input.php
@@ -1,15 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: falcon
- * Date: 1/22/18
- * Time: 8:37 PM
- */
 
+include('header.php');
 
-if(isset($_GET['player'])){
+?>
+<input type="text" list="characters" />
+<datalist id="characters">
+    <option>Alien</option>
+    <option>Bo Rai Cho</option>
+    <option>Cassie Cage</option>
+    <option>DVorah</option>
+    <option>Ermac</option>
+    <option>Erron Black</option>
+    <option>Ferra/Torr</option>
+    <option>Goro</option>
+    <option>Jacqui Briggs</option>
+    <option>Jason Voorhees</option>
+    <option>Jax</option>
+    <option>Johnny Cage</option>
+    <option>Kano</option>
+    <option>Kenshi</option>
+    <option>Kitana</option>
+    <option>Kotal Kahn</option>
+    <option>Kung Jin</option>
+    <option>Kung Lao</option>
+    <option>Leatherface</option>
+    <option>Liu Kang</option>
+    <option>Mileena</option>
+    <option>Predator</option>
+    <option>Quan Chi</option>
+    <option>Raiden</option>
+    <option>Reptile</option>
+    <option>Scorpion</option>
+    <option>Shinnok</option>
+    <option>Sonya Blade</option>
+    <option>Sub-Zero</option>
+    <option>Takeda</option>
+    <option>Tanya</option>
+    <option>Tremor</option>
+    <option>Triborg</option>
 
-}
-else{
-    echo "nobody here!";
-}
+</datalist>
