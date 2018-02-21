4e03e74280b3e1f88abcef8be118fe07dd89a085
Unknown <mhm.sherpa@gmail.com>
2018-02-20 23:38:21 -0600

    .

--- Scoreboard.php
+++ Scoreboard.php
@@ -7,14 +7,11 @@ echo("huh...");
 
 
 
-function buildScore(){
+
     $datafile = fopen("data.json", "r");
     echo fread($datafile,filesize("data.json"));
     echo "That iddnt work!";
-}
-function getNewGames(){
 
-}
 
 ?>
 
