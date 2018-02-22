5fd552b7dee371b2a0296baed5bab0fbf801c892
Unknown <mhm.sherpa@gmail.com>
2018-02-22 11:19:03 -0600

    .

--- scoreboard.php
+++ scoreboard.php
@@ -6,14 +6,14 @@ echo 'heeeeeeyyyyyy2';
 
 $gamedata = getGames();
 
-//printLatestGamesTable($gamedata);
+printLatestGamesTable($gamedata);
 
 
 
 
 function getGames(){
     $datafile = fopen("data.json", "r") or die("Unable to open file!");
-    $data_json = fread($datafile,filesize("data.json")) or die('unable to read data!');
+    $data_json= file_get_contents('data.json') or die("Unable to get contents!");
     fclose($datafile);
     $data = json_decode($data_json);
     print_r($data);
