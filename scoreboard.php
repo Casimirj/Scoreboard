c06fd43c94cfd78f8fe9625b4ba48875559e1b6c
Unknown <mhm.sherpa@gmail.com>
2018-02-22 12:56:10 -0600

    added 'score' to result array

--- scoreboard.php
+++ scoreboard.php
@@ -34,8 +34,11 @@ function printLatestGamesTable($gamedata){
     echo '</table>';
 }
 function generateScores($gamedata){
+    $jscore = array('Alien' => 0,'Bo Rai Cho' => 0,'Cassie Cage' => 0,'DVorah' => 0,'Ermac' => 0,'Erron Black' => 0,'Ferra/Torr' => 0,'Goro' => 0,'Jacqui Briggs' => 0,'Jason Voorhees' => 0,'Jax' => 0,'Johnny Cage' => 0,'Kano' => 0,'Kenshi' => 0,'Kitana' => 0,'Kotal Kahn' => 0,'Kung Jin' => 0,'Kung Lao' => 0,'Leatherface' => 0,'Liu Kang' => 0,'Mileena' => 0,'Predator' => 0,'Quan Chi' => 0,'Raiden' => 0,'Reptile' => 0,'Scorpion' => 0,'Shinnok' => 0,'Sonya Blade' => 0,'Sub-Zero' => 0,'Takeda' => 0,'Tanya' => 0,'Tremor' => 0,'Triborg' => 0);
     $jwins = array('Alien' => 0,'Bo Rai Cho' => 0,'Cassie Cage' => 0,'DVorah' => 0,'Ermac' => 0,'Erron Black' => 0,'Ferra/Torr' => 0,'Goro' => 0,'Jacqui Briggs' => 0,'Jason Voorhees' => 0,'Jax' => 0,'Johnny Cage' => 0,'Kano' => 0,'Kenshi' => 0,'Kitana' => 0,'Kotal Kahn' => 0,'Kung Jin' => 0,'Kung Lao' => 0,'Leatherface' => 0,'Liu Kang' => 0,'Mileena' => 0,'Predator' => 0,'Quan Chi' => 0,'Raiden' => 0,'Reptile' => 0,'Scorpion' => 0,'Shinnok' => 0,'Sonya Blade' => 0,'Sub-Zero' => 0,'Takeda' => 0,'Tanya' => 0,'Tremor' => 0,'Triborg' => 0);
     $jlosses = array('Alien' => 0,'Bo Rai Cho' => 0,'Cassie Cage' => 0,'DVorah' => 0,'Ermac' => 0,'Erron Black' => 0,'Ferra/Torr' => 0,'Goro' => 0,'Jacqui Briggs' => 0,'Jason Voorhees' => 0,'Jax' => 0,'Johnny Cage' => 0,'Kano' => 0,'Kenshi' => 0,'Kitana' => 0,'Kotal Kahn' => 0,'Kung Jin' => 0,'Kung Lao' => 0,'Leatherface' => 0,'Liu Kang' => 0,'Mileena' => 0,'Predator' => 0,'Quan Chi' => 0,'Raiden' => 0,'Reptile' => 0,'Scorpion' => 0,'Shinnok' => 0,'Sonya Blade' => 0,'Sub-Zero' => 0,'Takeda' => 0,'Tanya' => 0,'Tremor' => 0,'Triborg' => 0);
+
+    $dscore = array('Alien' => 0,'Bo Rai Cho' => 0,'Cassie Cage' => 0,'DVorah' => 0,'Ermac' => 0,'Erron Black' => 0,'Ferra/Torr' => 0,'Goro' => 0,'Jacqui Briggs' => 0,'Jason Voorhees' => 0,'Jax' => 0,'Johnny Cage' => 0,'Kano' => 0,'Kenshi' => 0,'Kitana' => 0,'Kotal Kahn' => 0,'Kung Jin' => 0,'Kung Lao' => 0,'Leatherface' => 0,'Liu Kang' => 0,'Mileena' => 0,'Predator' => 0,'Quan Chi' => 0,'Raiden' => 0,'Reptile' => 0,'Scorpion' => 0,'Shinnok' => 0,'Sonya Blade' => 0,'Sub-Zero' => 0,'Takeda' => 0,'Tanya' => 0,'Tremor' => 0,'Triborg' => 0);
     $dwins = array('Alien' => 0,'Bo Rai Cho' => 0,'Cassie Cage' => 0,'DVorah' => 0,'Ermac' => 0,'Erron Black' => 0,'Ferra/Torr' => 0,'Goro' => 0,'Jacqui Briggs' => 0,'Jason Voorhees' => 0,'Jax' => 0,'Johnny Cage' => 0,'Kano' => 0,'Kenshi' => 0,'Kitana' => 0,'Kotal Kahn' => 0,'Kung Jin' => 0,'Kung Lao' => 0,'Leatherface' => 0,'Liu Kang' => 0,'Mileena' => 0,'Predator' => 0,'Quan Chi' => 0,'Raiden' => 0,'Reptile' => 0,'Scorpion' => 0,'Shinnok' => 0,'Sonya Blade' => 0,'Sub-Zero' => 0,'Takeda' => 0,'Tanya' => 0,'Tremor' => 0,'Triborg' => 0);
     $dlosses = array('Alien' => 0,'Bo Rai Cho' => 0,'Cassie Cage' => 0,'DVorah' => 0,'Ermac' => 0,'Erron Black' => 0,'Ferra/Torr' => 0,'Goro' => 0,'Jacqui Briggs' => 0,'Jason Voorhees' => 0,'Jax' => 0,'Johnny Cage' => 0,'Kano' => 0,'Kenshi' => 0,'Kitana' => 0,'Kotal Kahn' => 0,'Kung Jin' => 0,'Kung Lao' => 0,'Leatherface' => 0,'Liu Kang' => 0,'Mileena' => 0,'Predator' => 0,'Quan Chi' => 0,'Raiden' => 0,'Reptile' => 0,'Scorpion' => 0,'Shinnok' => 0,'Sonya Blade' => 0,'Sub-Zero' => 0,'Takeda' => 0,'Tanya' => 0,'Tremor' => 0,'Triborg' => 0);
 
@@ -50,8 +53,11 @@ function generateScores($gamedata){
         $dlosses[$datarow['james']['character']] += $datarow['james']['score'];
         $jlosses[$datarow['daniel']['character']] += $datarow['daniel']['score'];
         $dlosses[$datarow['daniel']['character']] += $datarow['james']['score'];
+
+        $jscore[$datarow['james']['character']] = $jwins[$datarow['james']['character']] - $jlosses[$datarow['james']['character']];
+        $dscore[$datarow['daniel']['character']] = $dwins[$datarow['daniel']['character']] - $dlosses[$datarow['daniel']['character']];
     }
-    $scores = array('jwins'=>$jwins, 'jlosses'=>$jlosses, 'dwins'=>$dwins, 'dlosses'=>$dlosses);
+    $scores = array('jscore'=>$jscore, 'jwins'=>$jwins, 'jlosses'=>$jlosses, 'dscore'=>$dscore, 'dwins'=>$dwins, 'dlosses'=>$dlosses);
     print_r($scores);
     return $scores;
 }
