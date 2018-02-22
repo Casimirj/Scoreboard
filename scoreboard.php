c89ba0ebe60bd922a8475fa9d049e716b76561e5
Unknown <mhm.sherpa@gmail.com>
2018-02-22 11:16:31 -0600

    .

--- scoreboard.php
+++ scoreboard.php
@@ -1,5 +1,16 @@
 <?php
 
+
+include('header.php');
+echo 'heeeeeeyyyyyy2';
+
+$gamedata = getGames();
+
+//printLatestGamesTable($gamedata);
+
+
+
+
 function getGames(){
     $datafile = fopen("data.json", "r") or die("Unable to open file!");
     $data_json = fread($datafile,filesize("data.json")) or die('unable to read data!');
@@ -12,27 +23,16 @@ function printLatestGamesTable($gamedata){
     ?><table class="latestgames"><?
     foreach($gamedata as $datarow){
         ?><tr>
-        <td><?$datarow['round']?></td>
-        <td><?$datarow['jamesCharacter']?></td>
-        <td><?$datarow['jamesScore']?></td>
-        <td><?$datarow['danielCharacter']?></td>
-        <td><?$datarow['danielScore']?></td>
+            <td><?$datarow['round']?></td>
+            <td><?$datarow['jamesCharacter']?></td>
+            <td><?$datarow['jamesScore']?></td>
+            <td><?$datarow['danielCharacter']?></td>
+            <td><?$datarow['danielScore']?></td>
 
         </tr><?
     }
     ?></table><?
 }
-
-
-include('header.php');
-echo 'heeeeeeyyyyyy2';
-
-$gamedata = getGames();
-
-printLatestGamesTable($gamedata);
-
-
-
 
 
 ?>
