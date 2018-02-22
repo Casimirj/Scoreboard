5a973271caacb80c5d01972ad30d519717ef7e5d
Unknown <mhm.sherpa@gmail.com>
2018-02-22 11:14:39 -0600

    .

--- scoreboard.php
+++ scoreboard.php
@@ -1,5 +1,28 @@
 <?php
 
+function getGames(){
+    $datafile = fopen("data.json", "r") or die("Unable to open file!");
+    $data_json = fread($datafile,filesize("data.json")) or die('unable to read data!');
+    fclose($datafile);
+    $data = json_decode($data_json);
+    print_r($data);
+    return $data;
+}
+function printLatestGamesTable($gamedata){
+    ?><table class="latestgames"><?
+    foreach($gamedata as $datarow){
+        ?><tr>
+        <td><?$datarow['round']?></td>
+        <td><?$datarow['jamesCharacter']?></td>
+        <td><?$datarow['jamesScore']?></td>
+        <td><?$datarow['danielCharacter']?></td>
+        <td><?$datarow['danielScore']?></td>
+
+        </tr><?
+    }
+    ?></table><?
+}
+
 
 include('header.php');
 echo 'heeeeeeyyyyyy2';
@@ -10,29 +33,6 @@ printLatestGamesTable($gamedata);
 
 
 
-
-function getGames(){
-    $datafile = fopen("data.json", "r") or die("Unable to open file!");
-    $data_json = fread($datafile,filesize("data.json")) or die('unable to read data!');
-    fclose($datafile);
-    $data = json_decode($data_json);
-    print_r($data);
-    return $data;
-}
-function printLatestGamesTable($gameData){
-    ?><table class="latestgames"><?
-    foreach($gameData as $row){
-        ?><tr>
-            <td><?$row['round']?></td>
-            <td><?$row['jamesCharacter']?></td>
-            <td><?$row['jamesScore']?></td>
-            <td><?$row['danielCharacter']?></td>
-            <td><?$row['danielScore']?></td>
-
-        </tr><?
-    }
-    ?></table><?
-}
 
 
 ?>
