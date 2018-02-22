03f54f12e65579f7a4c97fbf0604065848131834
Unknown <mhm.sherpa@gmail.com>
2018-02-22 11:06:08 -0600

    .

--- scoreboard.php
+++ scoreboard.php
@@ -1,11 +1,11 @@
 <?php
 
-
+echo 'heeeeeeyyyyyy1';
 include('header.php');
-
+echo 'heeeeeeyyyyyy2';
 
 $gamedata = getGames();
-echo 'heeeeeeyyyyyy';
+
 printLatestGamesTable($gamedata);
 
 
@@ -13,7 +13,7 @@ printLatestGamesTable($gamedata);
 
 function getGames(){
     $datafile = fopen("data.json", "r") or die("Unable to open file!");
-    $data_json = fread($datafile,filesize("data.json"));
+    $data_json = fread($datafile,filesize("data.json")) or die('unable to read data!');
     fclose($datafile);
     $data = json_decode($data_json);
     print_r($data);
