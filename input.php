816b421d7d390325a05574458441f30301d13309
Unknown <mhm.sherpa@gmail.com>
2018-02-21 18:19:29 -0600

    .

--- input.php
+++ input.php
@@ -13,14 +13,14 @@ if(isset($_POST['jamesCharacter'])){
     $james = array('character'=> $_POST['jamesCharacter'], 'score'=>$_POST['jamesscore']);
     $daniel = array('character'=>$_POST['danielCharacter'], 'score'=>$_POST['danielscore']);
     $game = array('round'=>1, 'james'=>$james, 'daniel'=>$daniel);
-    print_r($game);
+    //print_r($game);
 
     array_push($scores, $game);
 
     $scores_json = json_encode($scores);
     fwrite($data, $scores_json);
     fclose($data);
-    //print_r($scores);
+    print_r($scores);
 
 }
 
